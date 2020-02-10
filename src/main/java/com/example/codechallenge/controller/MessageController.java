756adf247c81350642bb82f350172ce54145d08a
Ryan Forbes <rforbes@cardfree.com>
2020-02-10 11:59:13 -0700

    add age parameter to return messages newer than now minus `age` days ago

--- src/main/java/com/example/codechallenge/controller/MessageController.java
+++ src/main/java/com/example/codechallenge/controller/MessageController.java
@@ -4,7 +4,6 @@ import com.example.codechallenge.model.domain.Message;
 import com.example.codechallenge.model.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -13,7 +12,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 @RestController
 public class MessageController {
@@ -23,13 +24,18 @@ public class MessageController {
     @RequestMapping("/users/{userId}/messages")
     public Page<Message> showAll(@PathVariable(value = "userId") Long userId,
                                  @RequestParam(value = "senderId", required = false) Long senderId,
+                                 @RequestParam(value = "age", required = false, defaultValue = "30") Integer age,
                                  @PageableDefault(page = 0, size = 100)
                                      @SortDefault.SortDefaults({ @SortDefault(sort = "createdAt", direction = Sort.Direction.DESC) })
                                              Pageable pageable) {
+
+        LocalDateTime thirtyDaysAgo = LocalDateTime.now().minusDays(age);
+        Date since = Date.from( thirtyDaysAgo.atZone( ZoneId.systemDefault()).toInstant());
+
         if (senderId == null) {
-            return messageRepository.findAllByReceiverId(userId, pageable);
+            return messageRepository.findAllByReceiverIdAndCreatedAtGreaterThan(userId, since, pageable);
         } else {
-            return messageRepository.findAllByReceiverIdAndSenderId(userId, senderId, pageable);
+            return messageRepository.findAllByReceiverIdAndSenderIdAndCreatedAtGreaterThan(userId, senderId, since, pageable);
         }
     }
 
--- src/main/java/com/example/codechallenge/model/repository/MessageRepository.java
+++ src/main/java/com/example/codechallenge/model/repository/MessageRepository.java
@@ -6,11 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
-    Page<Message> findAllByReceiverId(Long id, Pageable pageable);
-    Page<Message> findAllByReceiverIdAndSenderId(Long receiverId, Long senderId, Pageable pageable);
+    Page<Message> findAllByReceiverIdAndCreatedAtGreaterThan(Long id, Date date, Pageable pageable);
+    Page<Message> findAllByReceiverIdAndSenderIdAndCreatedAtGreaterThan(Long receiverId, Long senderId, Date date, Pageable pageable);
 }
